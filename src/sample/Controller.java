254be65a2f12a2b4ccce334ee4c31a05311ad67c
BT_1N3_28 <nsakib6685@bths.edu>
2019-02-01 10:02:05 -0500

    Controller Update 2/1/2019 10:01 AM NS

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,24 @@
 package sample;
 
-import javafx.scene.control.ProgressBar;
+import java.awt.*;
 
 public class Controller {
 
-    ProgressBar progressBar1 = new ProgressBar();
+    // Manual Input
+    TextField question = new TextField();
+    TextField choice1 = new TextField();
+    TextField choice2 = new TextField();
+    TextField choice3 = new TextField();
 
-    public void updateBar()
-    {
-        
-    }
+    
+
+
+
+
+
+
+
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Swift");
         primaryStage.setScene(new Scene(root, 648, 467));
         primaryStage.show();
     }
