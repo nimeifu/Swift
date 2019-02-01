629d07bdb36a4a10841c79923eb8ce786a4dca66
BT_1N3_28 <nsakib6685@bths.edu>
2019-02-01 10:14:27 -0500

    Controller Update 2/1/2019 10:14 AM NS

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,12 +5,26 @@ import java.awt.*;
 public class Controller {
 
     // Manual Input
-    TextField question = new TextField();
-    TextField choice1 = new TextField();
-    TextField choice2 = new TextField();
-    TextField choice3 = new TextField();
+    TextField question;
+    TextField choice1;
+    TextField choice2;
+    TextField choice3 ;
+    Button manualSubmit;
+    String questionInput;
+    String choiceInput1;
+    String choiceInput2;
+    String choiceInput3;
 
-    
+    public void getManualInput()
+    {
+        questionInput = question.getText();
+        choiceInput1 = choice1.getText();
+        choiceInput2 = choice2.getSelectedText();
+        choiceInput3 = choice3.getSelectedText();
+        choice1.setText("lol");
+        System.out.println(questionInput+", "+choiceInput1+", "+choiceInput2+", "+choiceInput3);
+    }
+
 
 
 
