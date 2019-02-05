0999a2e8398598100a9aead96d5c8fd102cfb668
nimeifu <gliu3637@bths.edu>
2019-02-05 11:04:58 -0500

    getImagePath method done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.ProgressBar;
 
 import java.awt.*;
@@ -7,10 +8,12 @@ import java.awt.*;
 public class Controller {
 
     // Manual Input
+    @FXML
     TextField question;
     TextField choice1;
     TextField choice2;
-    TextField choice3 ;
+    TextField choice3;
+    TextField path;
     Button manualSubmit;
     String questionInput;
     String choiceInput1;
@@ -20,39 +23,37 @@ public class Controller {
     ProgressBar progresstwo;
     ProgressBar progressthree;
 
-    public void getManualInput()
-    {
+    public void getManualInput() {
         questionInput = question.getText();
         choiceInput1 = choice1.getText();
         choiceInput2 = choice2.getSelectedText();
         choiceInput3 = choice3.getSelectedText();
         choice1.setText("lol");
-        System.out.println(questionInput+", "+choiceInput1+", "+choiceInput2+", "+choiceInput3);
+        System.out.println(questionInput + ", " + choiceInput1 + ", " + choiceInput2 + ", " + choiceInput3);
     }
 
-    public void setProgressBarOne(double percent)
-    {
+    public void setProgressBarOne(double percent) {
         progressone.setProgress(percent);
     }
 
-    public void setProgressBarTwo(double percent)
-    {
+    public void setProgressBarTwo(double percent) {
         progresstwo.setProgress(percent);
     }
 
-    public void setProgressBarThree(double percent)
-    {
+    public void setProgressBarThree(double percent) {
         progressthree.setProgress(percent);
     }
 
-    public void resetAll()
-    {
+    public void resetAll() {
         progressone.setProgress(0);
         progresstwo.setProgress(0);
         progressthree.setProgress(0);
     }
 
-
-
-
+    public String getImagePath() {
+        return (path.getText());
+    }
 }
+
+
+
